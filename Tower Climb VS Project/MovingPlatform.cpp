7b667e62bf730da8f0a0cb0d70039559a2fa5257
doctorjaffa <nathanharris1502@gmail.com>
2023-03-23 13:00:21 +0000

    Start to add MovingPlatform logic

---
+++ Tower Climb VS Project/MovingPlatform.cpp
@@ -0,0 +1,49 @@
+#include "MovingPlatform.h"
+#include "AssetManager.h"
+#include "VectorHelper.h"
+
+MovingPlatform::MovingPlatform(sf::Vector2f newPos1, sf::Vector2f newPos2)
+	: Platform()
+	, SPEED()
+	, POS1(newPos1)
+	, POS2(newPos2)
+	, targetPoint(&POS2)
+	, velocity(0, 0)
+{
+	sprite.setTexture(AssetManager::RequestTexture("Assets/Graphics/MovingPlatform.png"));
+}
+
+void MovingPlatform::Update(sf::Time frameTime)
+{
+	float frameSeconds = frameTime.asSeconds();
+
+	sf::Vector2f newPos = GetPosition();
+
+	sf::Vector2f toMove = (velocity * frameSeconds);
+	float squareDistToMove = VectorHelper::SquareMagnitude(toMove);
+
+	sf::Vector2f vectorToTarget = *targetPoint - newPos;
+	float squareDistToTarget = VectorHelper::SquareMagnitude(vectorToTarget);
+
+	if (squareDistToMove >= squareDistToTarget)
+	{
+		// We arrived
+
+		// Set us to be on the target point
+		newPos = *targetPoint;
+
+		// Change our target point to be the other one
+		if (targetPoint == &POS1)
+			targetPoint = &POS2;
+		else
+			targetPoint = &POS1;
+
+		// Update our velocity 
+		sf::Vector2f vectorToNewTarget = *targetPoint - newPos;
+		
+	}
+
+	newPos += toMove;
+
+	
+}
---
+++ Tower Climb VS Project/MovingPlatform.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Platform.h"
+class MovingPlatform :
+    public Platform
+{
+public:
+
+    MovingPlatform(sf::Vector2f newPos1, sf::Vector2f newPos2);
+
+    void Update(sf::Time frameTime) override;
+
+private:
+
+    const float SPEED;
+    const sf::Vector2f POS1;
+    const sf::Vector2f POS2;
+    const sf::Vector2f* targetPoint;
+    sf::Vector2f velocity;
+};
+
